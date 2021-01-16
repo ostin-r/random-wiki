e3349ba26d66fd55629e6c5f40386dac960b6ff0
austinare <austinjrichards@outlook.com>
2021-01-16 11:23:15 -0700

    feeling the water for wikipedia API

--- random-wiki.py
+++ random-wiki.py
@@ -2,8 +2,8 @@ import wikipedia as wky
 
 # get a random wiki page
 rand_title = wky.random(pages=1)
-print(rand_title)
 
-# print the link to the user
+# print name and summary to the user
 rand_page = wky.WikipediaPage(title=rand_title).summary
+print(rand_title)
 print(rand_page)
