245c15eb70a28c4be4a23aaf7b9ad5071dfcd007
austinare <austinjrichards@outlook.com>
2021-01-18 17:06:29 -0700

    finished UI and basic functionality

--- random-wiki.py
+++ random-wiki.py
@@ -1,11 +1,34 @@
 import wikipedia as wky
+from tkinter import *
+from tkinter import ttk as tk
+from ttkthemes import ThemedTk
+import webbrowser
 
-# get a random wiki page
-rand_title = wky.random(pages=1)
+gui = ThemedTk(theme="clearlooks")
+gui.title("Random Wikipedia Page")
 
-# print name and summary to the user
-rand_page = wky.WikipediaPage(title=rand_title).summary
-print(rand_title)
-print(rand_page)
+# button commands
+def gen_page():
+    rand_title = wky.random(pages=1)
+    display.delete(0, END)
+    display.insert(0, str(rand_title))
 
-#yeet
+def open_page():
+    title = str(display.get())
+    link  = "https://en.wikipedia.org/wiki/" + title
+    webbrowser.open(link, new=1)
+
+
+# make a search box
+display = tk.Entry(width=80)
+display.grid(row=0, column=0)
+display.insert(0, "Enter a wikipedia page you would like to go to and press \"Go To Page\"")
+
+# buttons
+rand_button = tk.Button(gui, text="Generate Random Page", command=gen_page)
+open_button = tk.Button(gui, text="Go To Page", command=open_page)
+
+rand_button.grid(row=2, column=0, sticky="nsew")
+open_button.grid(row=3, column=0, stick="nsew")
+
+gui.mainloop()
